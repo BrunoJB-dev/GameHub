663108064785fe74c06bca0d33d5824dd5d619c7
BrunoJB <bruno.jean-baptiste@oclock.school>
2024-07-09 11:29:37 +0200

    Etape 4 ok

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const games = require(`${__dirname}/games.json`);
+console.log(games);
 
 app.use(express.static('public'));
 
@@ -13,13 +15,23 @@ app.get("/", (req, res) => {
     res.status(200).render("index", {});
 });
 
-app.get("/fourchette", (req, res) => {
+/*app.get("/fourchette", (req, res) => {
     res.status(200).render("fourchette", {});
-});
+});*/
 
-app.get("/diceroller", (req, res) => {
+app.get("/:gameName", (req,res) => {
+    const gameName = req.params.gameName;
+    const game = games.find(gamesEl => gamesEl.name.toLocaleLowerCase() === gameName);
+    if(game){
+      res.status(200).render(game.name, {game});
+    }else{
+      res.status(404).render("error", {message: "Game not found"});
+    }
+  });
+
+/*app.get("/diceroller", (req, res) => {
     res.status(200).render("diceRoller", {});
-});
+});*/
 
 const port = 3000;
 app.listen(port, () => {
